c15d8e380642d60241f383247534f9733afe7d90
German Almonte <german.a.almonte@gmail.com>
2022-01-11 12:46:38 -0500

    form data is getting through!

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+
+
 // Middlewear
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
 
 // Routes
-
 app.use('/places', require('./controllers/places'))
-app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req,res) => {
     res.render('home')
