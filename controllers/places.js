a3286a34d9b50175d11340516041ed5b316d7f7f
German Almonte <german.a.almonte@gmail.com>
2022-01-11 14:03:40 -0500

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -11,6 +11,20 @@ router.get('/', (req, res) => {
   res.render('places/index', {places})
 })
 
+//GET SHOW
+router.get('/:id', (req,res) =>{
+  res.render('places/show')
+  if (isNaN(id)){
+    res.render('error404')
+  }
+  else if(!places[id]){
+    res.render('places.show', {place: places[id]})
+  }
+  else{
+    res.render('places/show')
+  }
+})
+
 // POST PLACES
 router.post('/', (req, res) => {
   if (!req.body.pic){
