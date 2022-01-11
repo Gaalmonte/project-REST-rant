aba1463f3d715a1889ac82f7218b71def9064956
German Almonte <german.a.almonte@gmail.com>
2022-01-11 12:06:47 -0500

    Jazzed up form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -3,7 +3,33 @@ const Def = require('../default')
 
 function new_form (){
     return (
-        <Def><main><h1>Add a New Place</h1></main></Def>
+        <Def><main>
+            <h1>Add a New Place</h1>
+            <form method="POST" action="/places">
+                <div className="form-group">
+                    <label htmlFor="name">Place Name</label>
+                    <input className="form-control" id="name" name="name" required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="pic">Place Picture</label>
+                    <input className="form-control" type="url" id="pic" name="pic"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="city">City</label>
+                    <input className="form-control" id="city" name="city"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="pic">State</label>
+                    <input className="form-control" id="state" name="state"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="pic">Cuisines</label>
+                    <input className="form-control" id="cuisine" name="cuisine" required/>
+                </div>
+                <input className="btn btn-primary" type="submit" value="add Place" />
+            </form>
+            </main>
+        </Def>
     )
 }
 module.exports = new_form
