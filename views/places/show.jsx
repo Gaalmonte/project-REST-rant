d61ad15cfbbabf767c23be0f4a25b43c4794728e
German Almonte <german.a.almonte@gmail.com>
2022-01-15 15:53:11 -0500

    Added comments and comment form!

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -40,9 +40,43 @@ function show (data) {
                             </div>
                         </div>
                         <div className="row3">
-                            <h3 className="bg-info">Comments</h3>                            
+                            <h3 className="bg-info">Comments</h3>
+                            {comments}                            
                         </div>
-                        {comments}
+
+
+                        
+                        <h4 className="bg-info">Add your own comment!</h4>
+                        <form action={`/places/${data.place.id}/comment`} method="POST">
+                            <div class="form-group row">
+                                <label  class="col-sm-2 col-form-label">Name: </label>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control" id="author" name="author" placeholder="Jane Doe"/>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Rating: </label>
+                                <div class="col-md-8">
+                                    <input type="range" step="0.5" min="1" max="5" name="stars" id="stars"/>   
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                             <label  class="col-sm-2 col-form-label">Is this a rant? </label>
+                             <div class="col-sm-1">
+                                <div class="form-check">
+                                    <input type="checkbox" id="rant" name="rant" className="form-control" />
+                                </div>
+                             </div>
+                            </div>
+                            <div class="form-group row">
+                                <label  class="col-sm-2 col-form-label">Comment: </label>
+                                <div class="col-md-8">
+                                    <textarea class="form-control" id="content" name="content" rows="3" placeholder="I love this place! ..."></textarea>   
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+
                         {/* EDIT */}
                         <div>
                         <a href={`/places/${data.id}/edit`} className="btn btn-warning">
